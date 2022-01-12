00bbabe0d8ec447b0166826ddf9b9339c11a0d83
scheals <piecoo@gmail.com>
2022-01-12 12:14:19 +0100

    Introduce keyboard support Additionally change layout

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const calculatorButtons = document.querySelectorAll('.calc-button');
 const resultField = document.querySelector('.result-text');
 calculatorButtons.forEach(button => attachButtonListener(button));
+const restartCycle = document.querySelector('.restart-cycle');
+restartCycle.addEventListener('click', function () {recreateTheUniverse()});
+window.addEventListener('keydown', e => makeKeyboardFunctional(e.code));
+let hideCounter = 1;
+
 const Resulter ={
     operator : "",
     firstNumber: "",
@@ -8,93 +13,138 @@ const Resulter ={
     operate : 
     function () {
 
-    if ((typeof(+(this.firstNumber)) !== "number") || (typeof(+(this.secondNumber)) !== "number")) return console.log("One of the numbers ain't one.");
-    if (this.operator === "+") return add(+this.firstNumber, +this.secondNumber);
-    if (this.operator === "-") return subtract(+this.firstNumber, +this.secondNumber);
-    if (this.operator === "*") return multiply(+this.firstNumber, +this.secondNumber);
-    if (this.operator === "/") return divide(+this.firstNumber, +this.secondNumber);
-    return console.log("I didn't quite expect that.");    
+        if ((typeof(+(this.firstNumber)) !== "number") || (typeof(+(this.secondNumber)) !== "number")) return console.log("One of the numbers ain't one.");
+        if (this.operator === "+") return add(+this.firstNumber, +this.secondNumber);
+        if (this.operator === "-") return subtract(+this.firstNumber, +this.secondNumber);
+        if (this.operator === "*") return multiply(+this.firstNumber, +this.secondNumber);
+        if (this.operator === "/") return divide(+this.firstNumber, +this.secondNumber);
+        return console.log("I didn't quite expect that.");    
 
     },
 
 }
+
 function attachButtonListener(button){
-    return button.addEventListener('click', function() {makeFunctional(button.dataset.buttonSymbol)});
+    return button.addEventListener('click', function() {makeButtonsFunctional(button.dataset.buttonSymbol)});
 }
-function createDisplay(periodCheck = false){
-    if (Resulter.firstNumber.endsWith(".") && periodCheck === false) Resulter.firstNumber = Resulter.firstNumber.slice(0, Resulter.firstNumber.length-1);
-    if (Resulter.secondNumber.endsWith(".") && periodCheck === false) Resulter.secondNumber = Resulter.secondNumber.slice(0, Resulter.secondNumber.length-1);
-    return resultField.textContent = `${Resulter.firstNumber}` + " " + `${Resulter.operator}` + " " + `${Resulter.secondNumber}`;
-}
-function makeFunctional(buttonSymbol){
-    if (buttonSymbol === "reset") return resetDisplay();
-    if (buttonSymbol === "backspace") return removeLast();
-    if (buttonSymbol === "theme") return changeTheme();
-    if (buttonSymbol === "period" && Resulter.firstNumber.includes(".") === false && Resulter.firstNumber !== "" && Resulter.firstNumber !== "-" && Resulter.secondNumber === "" && Resulter.operator === "") {
-        Resulter.firstNumber += ".";
-        let dontCheckForPeriod = true;
-        return createDisplay(dontCheckForPeriod);
-    } else if  (buttonSymbol === "period" && Resulter.secondNumber.includes(".") === false && Resulter.secondNumber !== ""){
-        Resulter.secondNumber += ".";
-        let dontCheckForPeriod = true;
-        return createDisplay(dontCheckForPeriod);
-    }
-    if (buttonSymbol === "period") return console.log("That's enough periods.")
+
+function makeButtonsFunctional(buttonSymbol){
     switch (buttonSymbol) {
+        case "reset":
+            return resetDisplay();
+
+        case "backspace": 
+            return removeLast();
+
+        case "theme":
+            return changeTheme();
+
+        case "period":
+            if (Resulter.firstNumber.includes(".") === false && Resulter.firstNumber !== "" && Resulter.firstNumber !== "-" && Resulter.secondNumber === "" && Resulter.operator === "") {
+                Resulter.firstNumber += ".";
+                let dontCheckForPeriod = true;
+                return createDisplay(dontCheckForPeriod);
+            } else if  (Resulter.secondNumber.includes(".") === false && Resulter.secondNumber !== ""){
+                Resulter.secondNumber += ".";
+                let dontCheckForPeriod = true;
+                return createDisplay(dontCheckForPeriod);
+            }
+            if (buttonSymbol === "period") return console.log("That's enough periods.");
+
         case "add":
             if (Resulter.firstNumber === "" || Resulter.firstNumber === "-") return console.log("Forgot something?");
             Resulter.operator = "+";
             return createDisplay();
+
         case "subtract": 
             if (Resulter.firstNumber === "" || Resulter.firstNumber === "-") return console.log("Forgot something?");
             Resulter.operator = "-";
             return createDisplay();
+
         case "multiply": 
             if (Resulter.firstNumber === "" || Resulter.firstNumber === "-") return console.log("Forgot something?");
             Resulter.operator = "*";
             return createDisplay();
+
         case "divide": 
             if (Resulter.firstNumber === "" || Resulter.firstNumber === "-") return console.log("Forgot something?");
             Resulter.operator = "/";
             return createDisplay();
-    }
-    if (buttonSymbol === "sum") {
-        if (Resulter.secondNumber === "") return console.log("It takes two to tango.")
-        return Resulter.operate();
-    }
-    if (Resulter.operator === "") {
-        Resulter.firstNumber += buttonSymbol;
-        return createDisplay();
-    } else {
-        Resulter.secondNumber += buttonSymbol;
-        return createDisplay();
-    }
-}
 
-function resetDisplay() {
-    Resulter.firstNumber = "";
-    Resulter.secondNumber = "";
-    Resulter.operator = "";
-    createDisplay();
-    return console.log("Display has been cleared.");
-}
+        case "sum":
+            if (Resulter.secondNumber === "") return console.log("It takes two to tango.")
+            return Resulter.operate();
 
-function removeLast() {
-    let dontCheckForPeriod = true;
-    if (Resulter.secondNumber !== "") {
-       Resulter.secondNumber = Resulter.secondNumber.slice(0, Resulter.secondNumber.length-1);
-       return createDisplay(dontCheckForPeriod);
-
-        }else if (Resulter.operator !== "") {
-        Resulter.operator = "";
-        return createDisplay(dontCheckForPeriod);
-
-        } else {
-          Resulter.firstNumber = Resulter.firstNumber.slice(0, Resulter.secondNumber.length-1);
-          return createDisplay(dontCheckForPeriod);  
+        default: 
+            if (Resulter.operator === "") {
+                Resulter.firstNumber += buttonSymbol;
+                return createDisplay();
+            } else{
+                Resulter.secondNumber += buttonSymbol;
+                return createDisplay();
+            }
     }
 }
+function makeKeyboardFunctional(keyCode) {
+    switch (keyCode) {
+        case "Delete":
+            return resetDisplay();
 
+        case "Backspace": 
+            return removeLast();
+
+        case "ShiftRight":
+            return changeTheme();
+
+        case "NumpadDecimal":
+            if (Resulter.firstNumber.includes(".") === false && Resulter.firstNumber !== "" && Resulter.firstNumber !== "-" && Resulter.secondNumber === "" && Resulter.operator === "") {
+                Resulter.firstNumber += ".";
+                let dontCheckForPeriod = true;
+                return createDisplay(dontCheckForPeriod);
+            } else if  (Resulter.secondNumber.includes(".") === false && Resulter.secondNumber !== ""){
+                Resulter.secondNumber += ".";
+                let dontCheckForPeriod = true;
+                return createDisplay(dontCheckForPeriod);
+            }
+            if (keyCode === "NumpadDecimal") return console.log("That's enough periods.");
+
+        case "NumpadAdd":
+            if (Resulter.firstNumber === "" || Resulter.firstNumber === "-") return console.log("Forgot something?");
+            Resulter.operator = "+";
+            return createDisplay();
+
+        case "NumpadSubtract": 
+            if (Resulter.firstNumber === "" || Resulter.firstNumber === "-") return console.log("Forgot something?");
+            Resulter.operator = "-";
+            return createDisplay();
+
+        case "NumpadMultiply": 
+            if (Resulter.firstNumber === "" || Resulter.firstNumber === "-") return console.log("Forgot something?");
+            Resulter.operator = "*";
+            return createDisplay();
+
+        case "NumpadDivide": 
+            if (Resulter.firstNumber === "" || Resulter.firstNumber === "-") return console.log("Forgot something?");
+            Resulter.operator = "/";
+            return createDisplay();
+
+        case "NumpadEnter":
+            if (Resulter.secondNumber === "") return console.log("It takes two to tango.")
+            return Resulter.operate();
+             
+        default: 
+            if (Resulter.operator === "" && keyCode.includes('Numpad')) {
+                Resulter.firstNumber += keyCode.slice(keyCode.length-1, keyCode.length);
+                return createDisplay();
+            } else if (keyCode.includes('Numpad')){
+                Resulter.secondNumber += keyCode.slice(keyCode.length-1, keyCode.length);
+                return createDisplay();
+            } else {
+                return console.log("Whatever this is, I didn't expect it.")
+            }
+    }
+
+}
 function add(number, number2) {
     let result = number + number2;
     Resulter.secondNumber = "";
@@ -128,6 +178,36 @@ function divide(number, number2) {
     return createDisplay();
 }
 
+function createDisplay(periodCheck = false){
+    if (Resulter.firstNumber.endsWith(".") && periodCheck === false) Resulter.firstNumber = Resulter.firstNumber.slice(0, Resulter.firstNumber.length-1);
+    if (Resulter.secondNumber.endsWith(".") && periodCheck === false) Resulter.secondNumber = Resulter.secondNumber.slice(0, Resulter.secondNumber.length-1);
+    return resultField.textContent = `${Resulter.firstNumber}` + " " + `${Resulter.operator}` + " " + `${Resulter.secondNumber}`;
+}
+
+function resetDisplay() {
+    Resulter.firstNumber = "";
+    Resulter.secondNumber = "";
+    Resulter.operator = "";
+    createDisplay();
+    return console.log("Display has been cleared.");
+}
+
+function removeLast() {
+    let dontCheckForPeriod = true;
+    if (Resulter.secondNumber !== "") {
+       Resulter.secondNumber = Resulter.secondNumber.slice(0, Resulter.secondNumber.length-1);
+       return createDisplay(dontCheckForPeriod);
+
+        }else if (Resulter.operator !== "") {
+        Resulter.operator = "";
+        return createDisplay(dontCheckForPeriod);
+
+        } else {
+          Resulter.firstNumber = Resulter.firstNumber.slice(0, Resulter.secondNumber.length-1);
+          return createDisplay(dontCheckForPeriod);  
+    }
+}
+
 function destroySpaceTime() {
     const calculator = document.querySelector('.calculator');
     const body = document.querySelector('body');
@@ -146,16 +226,7 @@ function destroySpaceTime() {
     window.setTimeout(function () {body.appendChild(div)}, 42 * 250);
     return console.log("You really did!");
 }
-let hideCounter = 1;
-function hide(element) {
-    window.setTimeout(function () {element.classList.toggle('hide-me')}, 
-    hideCounter * 250);
-    hideCounter++;
-}
-function disappear(element) {
-    window.setTimeout(function () {element.classList.toggle('dont-show-me')}, 
-    hideCounter * 250);
-}
+
 function simulateSpaceTimeDestruction() {
     const endTimes = document.querySelector('.end-times');
     const calculator = document.querySelector('.calculator');
@@ -167,8 +238,7 @@ function simulateSpaceTimeDestruction() {
     hideCounter = 1;
     resetDisplay();
 }
-const restartCycle = document.querySelector('.restart-cycle');
-restartCycle.addEventListener('click', function () {recreateTheUniverse()});
+
 function recreateTheUniverse () {
     const endTimes = document.querySelector('.end-times');
     const calculator = document.querySelector('.calculator');
@@ -179,4 +249,15 @@ function recreateTheUniverse () {
     hide(calculator);
     thingsToHide.forEach(element => hide(element));
     hideCounter = 1;    
+}
+
+function hide(element) {
+    window.setTimeout(function () {element.classList.toggle('hide-me')}, 
+    hideCounter * 250);
+    hideCounter++;
+}
+
+function disappear(element) {
+    window.setTimeout(function () {element.classList.toggle('dont-show-me')}, 
+    hideCounter * 250);
 }
